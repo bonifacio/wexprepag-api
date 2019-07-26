09d83271f37816f8bb9abd8a38eff2b927dbf4d7
Bonifácio Júnior <boni.g.jr@gmail.com>
2019-07-26 19:11:49 -0300

    primeiro commit

---
+++ src/main/java/com/bonifacio/wexprepag/api/WexprepagApiApplication.java
@@ -0,0 +1,13 @@
+package com.bonifacio.wexprepag.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WexprepagApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WexprepagApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bonifacio/wexprepag/api/WexprepagApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bonifacio.wexprepag.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WexprepagApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
